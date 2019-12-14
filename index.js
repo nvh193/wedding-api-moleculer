989e989caf1c84c388ef235ff1b506615846a114
Huy Nguyen <viethuy11t1@gmail.com>
2019-12-14 22:42:29 +0700

    fix: Do not import attendace service

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { ServiceBroker } = require("moleculer");
 let api = require("./services/api.service");
 let flickr = require("./services/flickr.service");
 let gallery = require("./services/gallery.service");
+let attendance = require("./services/attendance.service");
 let config = require("./moleculer.config");
 
 const weddingBroker = new ServiceBroker(config);
@@ -10,5 +11,6 @@ const weddingBroker = new ServiceBroker(config);
 weddingBroker.createService(api);
 weddingBroker.createService(flickr);
 weddingBroker.createService(gallery);
+weddingBroker.createService(attendance);
 
 weddingBroker.start();
